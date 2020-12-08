6ee46157b5c406081d6fcf5c72d50132e20f95ad
Matheus Motta <matheusmottaq@gmail.com>
2020-12-07 22:17:46 -0300

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Head from 'next/head';
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <Head>
+        <title>Dev. Matheus Motta</title>
+      </Head>
+
+      <main>
+        <h1>Oi, Motta!</h1>
+      </main>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/styles/global.ts
@@ -0,0 +1,8 @@
+import { createGlobalStyles } from 'styled-components';
+
+export default createGlobalStyles`
+  * {
+    margin: 0;
+    padding: 0;
+  }
+`;
