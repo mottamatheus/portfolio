652bf32c1c89652495be89e5f5ada48bec52840b
Matheus MOtta <matheus-motta@matheus.motta>
2021-07-17 20:21:50 -0300

    feat: adiciona metatags

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import Head from 'next/head';
 
 type Props = {
   title: string;
-  keywords: string[];
+  description: string;
+  keywords: string;
 };
 
-export default function BlogPost({ ...props }: Props) {
-  return <div>oi</div>;
+export default function BlogPost({ title, description, keywords }: Props) {
+  return (
+    <>
+      <Head>
+        <title>{title} - Matheus Motta </title>
+        <meta name="description" content={description} />
+        <meta name="keywords" content={keywords} />
+      </Head>
+    </>
+  );
 }
